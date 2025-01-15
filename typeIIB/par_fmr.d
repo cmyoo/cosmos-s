2e8813d20dad170a11988ee52ca3c3a734aed87b
cmyoo <chulmoon.yoo@gmail.com>
2025-01-15 14:49:37 +0900

    [Add] typeIIB sample added

---
+++ typeIIB/par_fmr.d
@@ -0,0 +1,10 @@
+######################################################################
+##  parameters for FMR in COSMOS_S code 
+##   ver1.00 by Chulmoon Yoo  
+######################################################################
+##maximum number of fmr layers 
+0		
+##grid number for fmr region
+0
+##values of the lapse for starting fmr
+0.
